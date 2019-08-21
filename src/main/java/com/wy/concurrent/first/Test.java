67ced401716c2f986a3b4bde854974c0b9687e28
乌云 <wuydl@ibuscloud.com>
2019-08-21 13:07:08 +0800

    cancel annotation

--- src/main/java/com/wy/concurrent/first/Test.java
+++ src/main/java/com/wy/concurrent/first/Test.java
@@ -4,8 +4,8 @@ package com.wy.concurrent.first;
  * Created on 2019/8/21.
  * Title: Simple
  * Description:
- * Copyright: Copyright(c) 2018
- * Company: 杭州公共交通云科技有限公司
+ * Copyright: Copyright(c) 2019
+ * Company:
  *
  * @author wy
  */
