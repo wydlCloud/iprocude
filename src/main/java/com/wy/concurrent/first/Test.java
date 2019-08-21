fa6fd742314bd9ae171aab7495eefb6dfa7b7735
乌云 <wuydl@ibuscloud.com>
2019-08-21 13:05:29 +0800

    first commit

---
+++ src/main/java/com/wy/concurrent/first/Test.java
@@ -0,0 +1,13 @@
+package com.wy.concurrent.first;
+
+/**
+ * Created on 2019/8/21.
+ * Title: Simple
+ * Description:
+ * Copyright: Copyright(c) 2018
+ * Company: 杭州公共交通云科技有限公司
+ *
+ * @author wy
+ */
+public class Test {
+}
--- src/test/java/com/wy/concurrent/ConcurrentApplicationTests.java
+++ src/test/java/com/wy/concurrent/ConcurrentApplicationTests.java
@@ -1,4 +1,4 @@
-package com.wy.concurrent.concurrent;
+package com.wy.concurrent;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
