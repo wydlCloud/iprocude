fa42668ad1d033d9cac046bd639d6343541503a8
wy <529907231@qq.com>
2019-09-14 21:03:03 +0800

    designPattern

---
+++ src/main/java/com/wy/concurrent/designpattern/chainOfPattern/ChainPatternDemo.java
@@ -0,0 +1,24 @@
+package com.wy.concurrent.designpattern.chainOfPattern;
+
+/**
+ * Created on 2019/9/14.
+ * Title: Simple
+ * Description:
+ * Copyright: Copyright(c) 2019
+ * Company:
+ *
+ * @author wy
+ */
+public class ChainPatternDemo {
+    public static AbstractLogger getChainOfLoggers() {
+
+        AbstractLogger errorLogger = new ErrorLogger(AbstractLogger.ERROR);
+        AbstractLogger fileLogger = new FileLogger(AbstractLogger.DEBUG);
+        AbstractLogger consoleLogger = new ConsoleLogger(AbstractLogger.INFO);
+
+        errorLogger.setNextLogger(fileLogger);
+        fileLogger.setNextLogger(consoleLogger);
+
+        return  errorLogger;
+    }
+}
---
+++ src/main/java/com/wy/concurrent/designpattern/factory/Circle.java
@@ -0,0 +1,17 @@
+package com.wy.concurrent.designpattern.factory;
+
+/**
+ * Created on 2019/9/14.
+ * Title: Simple
+ * Description:
+ * Copyright: Copyright(c) 2019
+ * Company:
+ *
+ * @author wy
+ */
+public class Circle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Inside Circle::draw() method.");
+    }
+}
