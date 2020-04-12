794b2581a13cbed83a8b515b590b8457a992735d
wy <wuyun@mamahao.com>
2020-04-12 16:18:02 +0800

    hashMap的equals和hashcode

--- src/main/java/com/wy/concurrent/collection/hashmap/HashMapDemo.java
+++ src/main/java/com/wy/concurrent/collection/hashmap/HashMapDemo.java
@@ -15,11 +15,37 @@ import java.util.Map;
 public class HashMapDemo {
 
     public static void main(String[] args) {
-        Map<Integer,String> map=new HashMap<>();
-        map.put(1,"1"+1);
+//        Map<Integer,String> map=new HashMap<>();
+//        map.put(1,"1"+1);
      /* for (int i=0;i<=10000;i++){
           map.put(i,i+"---"+i);
       }
         System.out.println(map);*/
+     // 测试自定义类重写equals和hashcode值
+//        Student student=new Student();
+//        student.setName("小明");
+//        student.setAge(18);
+//        Student student1=new Student();
+//        student1.setName("小明");
+//        student1.setAge(18);
+//        HashMap<Student, String> map = new HashMap<>();
+//        map.put(student, "wy");
+//        map.put(student1, "yw");
+//        System.out.println(map.get(student));
+//        System.out.println(map.get(student1));
+//        System.out.println(student.equals(student1));
+
+        // 测试扩容resize
+        HashMap<Integer, Integer> integerIntegerHashMap=new HashMap<>();
+        for (int i=0;i<12;i++){
+            integerIntegerHashMap.put(i,i);
+        }
+        integerIntegerHashMap.put(12,12);
+        for (Integer x : integerIntegerHashMap.keySet()) {
+            System.out.println(x.intValue());
+        }
+
+
+
     }
 }
